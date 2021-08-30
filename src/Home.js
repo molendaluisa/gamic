62e6929700c4604d8274ca2d1828f70e2d841787
Alejandra Monge <ale.monge.gra@gmail.com>
2021-08-30 23:53:46 +0200

    Remove not used content

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from 'react-router-dom';
-import App from './App';
+// import App from './App';
 import AppM from './AppM';
 
 export default function Home() {
@@ -9,7 +9,7 @@ export default function Home() {
       <Switch>
         <Route path="/" component={AppM} exact />
         <Route path="/moderator" component={AppM} />
-        <Route path="/joinGame" component={App} />
+        {/* <Route path="/joinGame" component={App} /> */}
       </Switch>
     </main>
   );
