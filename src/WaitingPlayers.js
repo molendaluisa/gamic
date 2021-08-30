93465bf451a01a277e7c894bc6b7cab29767580c
Luisa Molenda <77063747+molendaluisa@users.noreply.github.com>
2021-08-30 22:33:43 +0200

    rules of the game added

--- src/WaitingPlayers.js
+++ src/WaitingPlayers.js
@@ -83,7 +83,9 @@ export default function WaitingPlayers(props) {
         <div className="waiting-center">
           {/* <p>Waiting for players...</p> */}
           <h2>Welcome!</h2>
-          <h3>Make yourself at home while we wait for players to join</h3>
+          <h3>Either <b>pancakes</b> or <b>waffles</b> are going to disappear from existence and your group needs to make a choice of which one to save:</h3>
+          <h3>- The votes will be via webcam, so please <b>turn your camera on</b> when you are asked to vote for your chosen option.</h3>
+          <h3>- When the Team decides, another option will be given to compete with the previous winner.</h3>
         </div>
         {/* } */}
 
@@ -97,7 +99,7 @@ export default function WaitingPlayers(props) {
             <li className="fot-li"><button className="btn btn-primary btn-start" onClick={handleStart}>Start</button></li>
           </ul>
         </footer>
-      </div>
+      </div >
     );
   }
 }
